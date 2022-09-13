5ee0603f60e3937a0d9ac4ca33e0ded950f940ff
El Vikingo <57925478+IsraelAnayaGT@users.noreply.github.com>
2022-09-13 17:31:03 -0500

    Detalles de producto

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarroIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
@@ -11,19 +11,19 @@ burguerMenu.addEventListener('click', toogleDeskMobileMenu);
 menuCarroIcon.addEventListener('click', toogleCarritoAside);
 
 function toogleDesktopMenu (){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-     aside.classList.add('inactive');  
+     shoppingCartContainer.classList.add('inactive');  
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleDeskMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-     aside.classList.add('inactive');  
+     shoppingCartContainer.classList.add('inactive');  
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -38,7 +38,7 @@ function toogleCarritoAside(){
      desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
